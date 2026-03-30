eda2ed4145924e4c55173deb043c73622a2a5293
Fredy <devfredylngt@gmail.com>
2026-03-31 00:40:25 +0300

    V 1.0.8 Updated serverlest function

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const app = require("./src/app");
 
-const port = 3000;
+// const port = 3000;
 
-if (process.env.NODE_ENV === "development") {
-  app.listen(port, () => {
-    console.log(`Server is listening on port ${3000}`);
-  });
-}
+// if (process.env.NODE_ENV === "development") {
+//   app.listen(port, () => {
+//     console.log(`Server is listening on port ${3000}`);
+//   });
+// }
 
 module.exports = app;
