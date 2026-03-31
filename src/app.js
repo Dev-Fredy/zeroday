3da8adacec0becb11489a2d3069bce4a901d4541
Fredy <devfredylngt@gmail.com>
2026-03-31 16:22:59 +0300

    V 1.0.9 updated ejs

--- src/app.js
+++ src/app.js
@@ -23,11 +23,9 @@ class App {
   }
 
   runApp() {
-    //after update
-   
     this.app.set("view engine", "ejs");
-     this.app.use(expressejs);
     this.app.set("views", path.join(__dirname, "views"));
+    this.app.use(expressejs);
     this.app.use(cookieParser());
     this.app.use(express.json());
 
